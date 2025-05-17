941f807f28a59056971b1e87b9b9f831cc87adee
Tom Barnes <tomtombarnes2@gmail.com>
2025-05-17 14:20:28 +0100

    updated

---
+++ aternos_server_manager.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium_stealth import stealth
+import time
+
+options = webdriver.ChromeOptions()
+options.add_argument("start-maximized")
+
+# options.add_argument("--headless")
+
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option('useAutomationExtension', False)
+driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
+
+stealth(driver,
+        languages=["en-US", "en"],
+        vendor="Google Inc.",
+        platform="Win32",
+        webgl_vendor="Intel Inc.",
+        renderer="Intel Iris OpenGL Engine",
+        fix_hairline=True,
+        )
+
+url = "https://aternos.org/server"
+driver.get(url)
+input()
+driver.quit()
